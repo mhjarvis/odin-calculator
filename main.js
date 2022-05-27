bf68aaf9eaa7f608dea22fbab1e191f42ccb5427
Markus Jarvis <markus.h.jarvis@gmail.com>
2022-05-27 11:18:06 -0500

    Add "operate" function that calls indiv. mathematical functions

--- main.js
+++ main.js
@@ -2,12 +2,19 @@
 
 function operate(num1, num2, operator) {
 
+    if(operator == '+') {
+        return add(num1, num2);
+    }
+    if(operator == '-') {
+        return subtract(num1, num2);
+    }
+    if(operator == '*') {
+        return multiply(num1, num2);
+    }
+    if(operator == '/') {
+        return divide(num1, num2);
+    }
 }
-
-
-
-
-
 
 
 
