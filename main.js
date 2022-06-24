4b4310ce609069076647d4aebce47b1faf31b849
Markus Jarvis <markus.h.jarvis@gmail.com>
2022-06-24 14:09:20 -0500

    Update functions to take all valules into array

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ let displayValue = 0;
 
 let firstNum = null;
 let waitingForSecondNum = true;
-let temp = [];
+let arrayOfValues = [];
+let tempNumber = [];
 let operator;
 
 
@@ -26,15 +27,66 @@ allOperatorButtons.forEach(op => {
 }) 
 
 // add numbers to array as they are clicked  
-
 function pushNumbersToArray(value) {
-    temp.push(value); 
-    updateDisplay(temp.join(""));
-    console.log(value); 
+    tempNumber.push(value);
+    updateDisplay(arrayOfValues.join("") + tempNumber.join(""));
 }
 
+// add operator to array or call equal function
 function useOperator(value) {
-    if(firstNum !== null) {
+
+    if(value == '=') {
+        arrayOfValues.push(tempNumber.join(""));
+        tempNumber = [];
+        startTheWaterfall();
+        return;
+    }
+
+    updateDisplay(arrayOfValues.join("") + tempNumber.join("") + value);
+
+    if(value == 'AC') {
+        resetVariables();
+    } else if(value != '=') {
+        if(tempNumber.length != 0) {    //only add number to array if there is a value in tempNumber
+            arrayOfValues.push(Number(tempNumber.join("")));
+        }
+        tempNumber = [];
+        arrayOfValues.push(value);
+    }
+    console.log(arrayOfValues);
+}
+
+function startTheWaterfall() {
+    updateDisplay(arrayOfValues);
+
+    let temp1;
+    let temp2;
+    let operator;
+    let waiting = false;
+
+    for(let i = 0; i < arrayOfValues.length; i++) {
+        if(Number(arrayOfValues[i]) && waiting == false) {
+            temp1 = arrayOfValues[i];
+        } else if(!Number(arrayOfValues[i])) {
+            operator = arrayOfValues[i];
+            waiting = true;
+        } else if(Number(arrayOfValues[i]) && waiting == true) {
+            temp2 = arrayOfValues[i];
+            temp1 = operate(Number(temp1), Number(temp2), operator);
+            temp2 = null;
+            operator = null;
+            waiting = false;
+        }
+
+        console.log(temp1);
+    }
+    arrayOfValues = [];
+    arrayOfValues.push(temp1);
+    updateDisplay(temp1);
+}
+
+ 
+/*     if(firstNum !== null) {
         console.log("minus");
         let secondNumber = Number(temp.join(""));
         firstNum = operate(firstNum, secondNumber, value);
@@ -46,13 +98,21 @@ function useOperator(value) {
         operator = value;
         temp = [];
         console.log("elseif value: " + value);
-    }
-}
+    } */
+
 
 // update main screen
 function updateDisplay(displayValue) {
     let screen = document.querySelector(".main-screen");
     screen.innerHTML = displayValue;
+}
+
+function resetVariables() {
+    firstNum = null;
+    waitingForSecondNum = true;
+    arrayOfValues = [];
+    operator = "";
+    updateDisplay(0);
 }
 
 
